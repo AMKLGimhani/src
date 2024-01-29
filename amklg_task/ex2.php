1024d4c18ecb7ef84294c857813bfc50910edfc3
Lakshika Gimhani <127098842+AMKLGimhani@users.noreply.github.com>
2024-01-29 11:25:26 +0200

    Ex2 done

---
+++ amklg_task/ex2.php
@@ -0,0 +1,14 @@
+<?php 
+$title = "Exercise2";
+include 'header.php'; ?>
+
+<body>
+
+<h1>In Finland, you’re never more than a short walk from nature</h1>
+        <p>Whether you want to escape to lush forests, adventure in Arctic wilderness or relax on open coasts, Finland has plenty of space for everyone to explore. Plus, the flora and fauna are like nowhere else on Earth – especially when viewed under the spectacular Northern Lights.</p>
+        <p>Finland has more than 40 national parks, and they’re scattered throughout the country’s islands, lakes, forests, peat lands and fells. There’s even one just 45 minutes from Helsinki’s city centre and airport. Each park showcases the local area’s environment, and many have landscapes that include rapids, rift valleys and eskers.</p>
+
+
+
+
+<?php include 'footer.php'; ?>
---
+++ amklg_task/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="row">
+            <div class="col-md-4">&copy; Hamk University</div>
+            <div class="col-md-4">Computer Application</div>
+            <div class="col-md-4">Hämeenlinna</div>
+    </div>
+</footer>
+
+          
+
+    </div> <!-- container -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+</body>
+</html>
