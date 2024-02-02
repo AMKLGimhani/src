65f197418c78574a58d2b1e44a6180437735f0d5
Lakshika Gimhani <127098842+AMKLGimhani@users.noreply.github.com>
2024-02-02 22:00:49 +0200

    ex 3

--- amklg_task/ex3.php
+++ amklg_task/ex3.php
@@ -2,8 +2,8 @@
 $title = "Exercise3: Variable, Strings & Operators";
 include 'header.php'; ?>
 
-<h1>Form Creation: Create a simple HTML form to collect the user’s Firstname and Lastname. Use the echo statement 
-    to print “Hello [Firstname] [Lastname], You are welcome to my site.” inside an <h3> tag.</h1>
+<h3>3.2: Form Creation: Create a simple HTML form to collect the user’s Firstname and Lastname. Use the echo statement 
+    to print “Hello [Firstname] [Lastname], You are welcome to my site.” inside an <h3> tag.</h3>
 
 <form action="process1.php" method="post">
     <div class="mb-3"> 
@@ -14,10 +14,67 @@ include 'header.php'; ?>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 
-</form>
+</form> <br>
 
-<h1>HTML Table: Prepare a simple HTML table and apply Bootstrap styles to it. 
-    Note, you have already done a table in Exercise 1. If you wish, you can use the same table. </h1>
+<h3>3.4: HTML Table: Prepare a simple HTML table and apply Bootstrap styles to it. 
+    Note, you have already done a table in Exercise 1. If you wish, you can use the same table. </h3>
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">S.N</th>
+      <th scope="col">Name</th>
+      <th scope="col">Grade</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>John</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Alice</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Bob</td>
+      <td>5</td>
+    </tr>
+  </tbody>
+</table> <br>
+
+<h3>3.5: String Variables: Write a PHP script with two string variables ($str1 = "Hello"; $str2 = "World";).
+     Join them together and print the length of the string.</h3>
+
+<?php
+$str1 = "Hello";
+$str2 = "World";
+echo $str1." ".$str2."<br>"; 
+echo strlen($str1." ".$str2);
+?> <br><br>
+
+<h3>3.6: Number Addition: Write a script to add up the numbers: 298, 234, 46. 
+    Use variables to store these numbers and an echo statement to output your answer.</h3>
+
+<?php
+$number1 = 298;
+$number2 = 234;
+$number3 = 46;
+
+$sum = $number1 + $number2 + $number3;
+$direct_text = "The sum is: ";
+
+echo ($direct_text . $sum);
+
+?> <br><br>
+
+<h3>3.7: Browser Detection: Write a PHP script to detect the browser being used to view your pages. 
+    (Hint: Use predefined variables: $_SERVER)</h3>
+
+
 
 
 <?php include 'footer.php'; ?>
